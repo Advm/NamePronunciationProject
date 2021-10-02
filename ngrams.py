6d7fec121ea43d531f840677905709d269714b92
jkaffenb <58486620+jkaffenb@users.noreply.github.com>
2021-10-02 15:52:20 -0400

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -24,17 +24,42 @@ def train_ngrams(list_str, n):
                 mydict.update({gram: num + 1})
     return mydict, population
 
+def ngrams_phoneme_algorithm(corpus, phoneme):
+    """ Given a corpus and a phoneme, *currently* compute the average bi-gram probability
+        of the word. """
+    bi_gram, bi_gram_pop = train_ngrams(corpus, 2)
+    un_gram, un_gram_pop = train_ngrams(corpus, 1)
+    word_bigrams = ngrams(phoneme, 2)
+
+    average_bigram_prob = 0
+    for gram in word_bigrams:
+        # If the corpus doesn't have this bi-gram, continue on to the next bi-gram. 
+        # Might need to change the weight of this later but for now it seems fine 
+        if bi_gram.get(gram) == None:
+            continue
+
+        average_bigram_prob += bi_gram.get(gram) / un_gram.get(gram[0])
+
+    # To make sure that the word isn't composed completely of bi-grams not found
+    # in the corpus
+    if average_bigram_prob != 0:
+        average_bigram_prob = average_bigram_prob / len(word_bigrams)
+    #average_corpus_prob = len(bi_gram) / un_gram_pop
+
+    return average_bigram_prob
+
+#Potential issue with words < the n length
 
 # Working through the examples given https://blog.xrds.acm.org/2017/10/introduction-n-grams-need/
 
 # (the number of times the previous word ‘wp’ occurs before the word ‘wn’) / 
 # (the total number of times the previous word ‘wp’ occurs in the corpus)
 
-# two_gram, y = train_ngrams(["abcd", "abefgahijk", "almno", "nopqr", "stuvnw"], 2)
-# one_gram, x = train_ngrams(["abcd", "abefgahijk", "almno", "nopqr", "stuvnw"], 1)
+#two_gram, y = train_ngrams(["abcd", "abefgahijk", "almno", "nopqr", "stuvnw"], 2)
+#one_gram, x = train_ngrams(["abcd", "abefgahijk", "almno", "nopqr", "stuvnw"], 1)
 
-# print(two_gram.get("cd") / one_gram.get("c")) -> 100% of the time
-# print(two_gram.get("no") / one_gram.get("n")) -> 67% of the time
+#print(two_gram.get("cd") / one_gram.get("c")) #-> 100% of the time
+#print(two_gram.get("no") / one_gram.get("n")) #-> 67% of the time
 
 
 
