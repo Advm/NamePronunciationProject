0a075f39ee9db046caa2a19fbc8128624fe7f520
KennyTalarico <kennytalarico@gmail.com>
2021-10-24 13:44:03 -0400

    changed to_ipa names

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import math
+from random import choice
 
 model = tf.keras.models.load_model('IsAmericanEnglish')
 
@@ -88,7 +89,7 @@ def main(words):
 
     # <ipa_names> is a list of the same length containing IPA transcriptions of each name
     #   i.e., ipa_names[i] is an IPA transcription of names[i]
-    ipa_names = [ipa_model.ipa(name)[1:-1] for name in names]
+    ipa_names = [ipa_model.to_ipa(name)[1:-1] for name in names]
     #print(ipa_names)
 
     # Get n-grams scores
@@ -104,6 +105,23 @@ def main(words):
 
     return pd.concat([pd.DataFrame(names), pd.DataFrame(final_scores)], axis=1, ignore_index=True)
 
+
+def testoutput():
+    with open("ipa_dicts/english-general_american.csv", encoding="utf8") as f:
+        reader = csv.reader(f)
+        corpus = [w[1:-1] for row in reader for w in row[1].split(', ')]
+    names = [choice(corpus) for _ in range(200)]
+    ipa_names = [ipa_model.ipa(name)[1:-1] for name in names]
+    ngrams_scores = [ngrams_phoneme_algorithm(name) for name in ipa_names]
+    nn_scores = getoutput(ipa_names, model)
+    final_scores = [round(((nn_scores[i] + ngrams_scores[i]) / 2) * 100, 2) for i in range(len(ngrams_scores))]
+    final_scores = [round(100 - x, 2) for x in final_scores]
+    with open("test-out.csv", 'w', encoding="utf8") as f:
+        writer = csv.writer(f)
+        writer.writerows([[names[i], final_scores[i]] for i in range(len(names))])
+
+
 if __name__ == '__main__':
-    root = Root_Win(main)
-    root.mainloop()
+    # root = Root_Win(main)
+    # root.mainloop()
+    testoutput()
--- to_ipa.py
+++ to_ipa.py
@@ -1,63 +1,65 @@
 from g2p_en import G2p
 from syllabifier import syllabifyARPA
+import sys
 
 class to_ipa:
     def __init__(self):
-        self.g2p = G2p()
-        self.map = {'AA': 'ɑ',
-                    'AE': 'æ',
-                    'AH': 'ə',
-                    'AO': 'ɔ',
-                    'AW': 'aʊ',
-                    'AY': 'aɪ',
-                    'B': 'b',
-                    'CH': 'tʃ',
-                    'D': 'd',
-                    'DH': 'ð',
-                    'EH': 'ɛ',
-                    'ER': 'ɝ',
-                    'EY': 'eɪ',
-                    'F': 'f',
-                    'G': 'ɡ',
-                    'HH': 'h',
-                    'IH': 'ɪ',
-                    'IY': 'i',
-                    'JH': 'dʒ',
-                    'K': 'k',
-                    'L': 'ɫ',
-                    'M': 'm',
-                    'N': 'n',
-                    'NG': 'ŋ',
-                    'OW': 'oʊ',
-                    'OY': 'ɔɪ',
-                    'P': 'p',
-                    'R': 'ɹ',
-                    'S': 's',
-                    'SH': 'ʃ',
-                    'T': 't',
-                    'TH': 'θ',
-                    'UH': 'ʊ',
-                    'UW': 'u',
-                    'V': 'v',
-                    'W': 'w',
-                    'Y': 'j',
-                    'Z': 'z',
-                    'ZH': 'ʒ'
-                    }
+        self.graph_to_phone_model = G2p()
+        self.arpabet_to_ipa_mapping = {'AA': 'ɑ',
+                                        'AE': 'æ',
+                                        'AH': 'ə',
+                                        'AO': 'ɔ',
+                                        'AW': 'aʊ',
+                                        'AY': 'aɪ',
+                                        'B': 'b',
+                                        'CH': 'tʃ',
+                                        'D': 'd',
+                                        'DH': 'ð',
+                                        'EH': 'ɛ',
+                                        'ER': 'ɝ',
+                                        'EY': 'eɪ',
+                                        'F': 'f',
+                                        'G': 'ɡ',
+                                        'HH': 'h',
+                                        'IH': 'ɪ',
+                                        'IY': 'i',
+                                        'JH': 'dʒ',
+                                        'K': 'k',
+                                        'L': 'ɫ',
+                                        'M': 'm',
+                                        'N': 'n',
+                                        'NG': 'ŋ',
+                                        'OW': 'oʊ',
+                                        'OY': 'ɔɪ',
+                                        'P': 'p',
+                                        'R': 'ɹ',
+                                        'S': 's',
+                                        'SH': 'ʃ',
+                                        'T': 't',
+                                        'TH': 'θ',
+                                        'UH': 'ʊ',
+                                        'UW': 'u',
+                                        'V': 'v',
+                                        'W': 'w',
+                                        'Y': 'j',
+                                        'Z': 'z',
+                                        'ZH': 'ʒ'
+                                        }
         self.stress = ['', 'ˈ', 'ˌ']
         
-    def ipa(self, graphemes):
+    def to_ipa(self, graphemes):
         """ Convert English graphemes to IPA. """
         word = "/"
-        arpa = self.g2p(graphemes)
+        arpa = self.graph_to_phone_model(graphemes)
         try:
             for syl in syllabifyARPA(arpa):
                 word += self.find_stress(syl)
                 for phone in syl.split(" "):
-                    word += self.map[phone[:2]]
+                    word += self.arpabet_to_ipa_mapping[phone[:2]]
         except:
+            print(f"Unable to find stress for word {graphemes}. Analyzing with no stress markers.", file=sys.stderr)
             for phone in arpa:
-                word += self.map[phone[:2]]
+                word += self.arpabet_to_ipa_mapping[phone[:2]]
         
         return word + '/'
 
